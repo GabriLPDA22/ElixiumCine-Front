4342434a41ba94d60ba93d197dd50d39d76be1a5
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-11 17:09:25 +0100

    mejora cartelera grancasa estilo villa

--- cine_web_app/back_end/obj/Debug/net6.0/CineWebApp.BackEnd.AssemblyInfo.cs
+++ cine_web_app/back_end/obj/Debug/net6.0/CineWebApp.BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineWebApp.BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b8884ba2115a369bf28a93c8aaa1f068b91262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483f7aa791875ceffe15adaed2768dc6419c83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineWebApp.BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineWebApp.BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
