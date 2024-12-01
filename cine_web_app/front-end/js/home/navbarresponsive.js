f07ef11ecb8a07886a7210a27bd606bfe740b674
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-12-01 23:46:18 +0100

    arreglo de boton home y navbar responsive nueva fucionalizad

--- cine_web_app/front-end/js/home/navbarresponsive.js
+++ cine_web_app/front-end/js/home/navbarresponsive.js
@@ -4,13 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function cargarElementos() {
-const responsiveHamburger = document.querySelector('.header--responsive .header__hamburger');
-const responsiveHeader = document.querySelector('.header--responsive');
-const body = document.body;
+    const responsiveHamburger = document.querySelector('.header--responsive .header__hamburger');
+    const responsiveHeader = document.querySelector('.header--responsive');
+    const body = document.body;
 
-responsiveHamburger.addEventListener('click', function () {
-    responsiveHeader.classList.toggle('is-open');
-    body.classList.toggle('menu-open'); // Evita el scroll cuando el menú está abierto
-});
+    // Abrir y cerrar el menú al hacer clic en el botón del hamburger
+    responsiveHamburger.addEventListener('click', function (e) {
+        e.stopPropagation(); // Impide que el clic en el botón se propague y cierre el menú
+        responsiveHeader.classList.toggle('is-open');
+        body.classList.toggle('menu-open'); // Evita el scroll cuando el menú está abierto
+    });
 
+    // Cerrar el menú si se hace clic fuera de él
+    document.addEventListener('click', function (e) {
+        if (!responsiveHeader.contains(e.target) && !responsiveHamburger.contains(e.target)) {
+            responsiveHeader.classList.remove('is-open');
+            body.classList.remove('menu-open');
+        }
+    });
+
+    // Evitar que el clic en el menú abra el menú al hacer clic dentro de él
+    responsiveHeader.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
 }
