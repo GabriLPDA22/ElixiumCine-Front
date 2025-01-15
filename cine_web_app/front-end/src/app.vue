e920d1ffadc340bef39961dd3dbc2ea74ce4c59f
GabriLPDA22 <gsaiz.bajo@gmail.com>
2025-01-15 18:11:26 +0100

    v1.0 con node modules

--- cine_web_app/front-end/src/app.vue
+++ cine_web_app/front-end/src/app.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import Ofertas from './components/Ofertas.vue';
 import Navbar from './components/navbar.vue';
+import CustomFooter from './components/CustomFooter.vue';
 </script>
 
 <template>
     <div>
-        <Navbar />
-        <Ofertas />
+        <header>
+            <Navbar />
+        </header>
+        <main>
+            <Ofertas />
+
+        </main>
+        <footer>
+            <CustomFooter />
+        </footer>
     </div>
 </template>
