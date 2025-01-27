c596efba2cdd2e96741cd0213651c4d8666196ae
GabriLPDA22 <gsaiz.bajo@gmail.com>
2025-01-27 17:33:36 +0100

    feat: Actualizar dependencia de Pinia a la versión 2.3.1

--- cine_web_app/front-end/src/main.ts
+++ cine_web_app/front-end/src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue';
 import App from './app.vue'; // Asegúrate de que esta ruta sea correcta
 import router from './router'; // Archivo router.ts donde definiste las rutas
+import { createPinia } from 'pinia';
 
 // Crear la instancia de la aplicación
 const app = createApp(App);
+const pinia = createPinia();
 
 // Usar el router en la aplicación
 app.use(router);
+app.use(pinia);
 
 // Montar la aplicación en el elemento HTML con id "app"
 app.mount('#app');
